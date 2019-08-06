0108b74034a66bb3663296439468630be9011ff6
Ogier Bouvier <ars3nic@ars3nic.ch>
2019-08-06 17:34:02 +0200

    update server for new API
    Signed-off-by: Ogier Bouvier <ars3nic@ars3nic.ch>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #[macro_use]
 extern crate error_chain;
 
+use std::net::Ipv4Addr;
 use std::ptr;
+use std::thread;
 
-use log::debug;
+use bounded_spsc_queue::{make, Consumer};
 
-use netif::{Runnable, HugeInfo};
-use netif::server::ServerContext;
+use log::{debug, info, error};
+
+use netif::*;
+use netif::server::*;
 
 use nix::fcntl::{open, OFlag};
 use nix::sys::mman::{mmap, munmap, MapFlags, ProtFlags};
@@ -22,6 +26,45 @@ error_chain! {
 
     links {
         NetIf(::netif::Error, ::netif::ErrorKind);
+    }
+}
+
+struct DummySched {
+    command: Consumer<SchedulerClosure>,
+}
+
+impl DummySched {
+    fn new(command: Consumer<SchedulerClosure>) -> Self {
+        Self {
+            command,
+        }
+    }
+}
+
+impl ServerScheduler for DummySched {
+    fn register(&mut self, pid: u32, id: u32, addr: Ipv4Addr, port: u16, proto: IpProto) {
+        info!("registering {}:{} as {} for client {}", addr, port, proto, pid);
+    }
+
+    fn add_in(&mut self, pid: u32, queue: ShmContainer<ShmPacket>) {
+        info!("adding input queue {} for client {}", pid, queue);
+    }
+
+    fn add_out(&mut self, pid: u32, queue: ShmContainer<ShmPacket>) {
+        info!("adding output queue {} for client {}", pid, queue);
+    }
+}
+
+impl Runnable for DummySched {
+    type Error = netif::Error;
+
+    fn run(&mut self) -> std::result::Result<(), Self::Error> {
+        loop {
+            if let Some(closure) = self.command.try_pop() {
+                info!("applying closure on scheduler");
+                (closure)(self)
+            }
+        }
     }
 }
 
@@ -42,11 +85,24 @@ fn main() -> Result<()> {
     )}?;
     let hp_info = vec![HugeInfo::new(base as *mut u8, LEN, f)];
 
-    for h in &hp_info {;
+    for h in &hp_info {
         debug!("{}", h);
     }
 
-    let mut ctx = ServerContext::new(hp_info)?;
+    // creating the scheduler command channel
+    let (prod, cons) = make(32);
+
+    let producers = vec![prod];
+
+
+    // spawning the dummy scheduler
+    thread::spawn(move || {
+        if let Err(e) = DummySched::new(cons).run() {
+            error!("scheduler failed: {}", e);
+        }
+    });
+
+    let mut ctx = ServerContext::new(hp_info, producers)?;
 
     ctx.run()?;
 
